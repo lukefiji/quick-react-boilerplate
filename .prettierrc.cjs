ed61dc25b0c599397e6845b44bbf3d2eb8399f39
Luke Fiji <luke.fiji@gmail.com>
2023-08-15 10:32:54 -0700

    Update prettierrc

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,10 +2,7 @@
 const config = {
   singleQuote: true,
   tailwindConfig: './tailwind.config.js',
-  plugins: [
-    'prettier-plugin-organize-imports',
-    require('prettier-plugin-tailwindcss'),
-  ],
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
 };
 
 module.exports = config;
