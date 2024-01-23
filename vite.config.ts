284b85b5ea324e2d08c478107fd1e647fcf93092
Luke Fiji <luke.fiji@gmail.com>
2024-01-22 20:58:20 -0800

    add @types/node and update imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path from 'node:path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
